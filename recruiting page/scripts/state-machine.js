6907a8c6e19cce3839dd240c34fcaf94ac490838
Codrin Mares <codrin@tactillo.com>
2023-07-14 17:20:22 +0300

    Second commit with content from previous contributor.

---
+++ recruiting page/scripts/state-machine.js
@@ -0,0 +1,84 @@
+const opening = document.getElementById('opening');
+const closingAnimation = document.getElementById('closingAnimation');
+const closing = document.getElementById('closing');
+const paper = document.getElementById('paperDiv');
+const envelope = document.getElementById('envelope');
+const source = document.getElementById('changeSrc');
+const wrapper = document.getElementById('wrapper');
+
+const btnSubmit = document.getElementById('btnSubmit');
+const btnPrev = document.getElementById('btnPrev');
+const btnNext = document.getElementById('btnNext');
+
+var isReplayDisplayed = true;
+
+opening.addEventListener('ended',middleSection,false);
+closingAnimation.addEventListener('ended',showRepeat,false);
+
+window.onresize = resize();
+
+function resize() {
+    if ( isReplayDisplayed ) {
+        const replayButton = document.getElementById('replayButton');
+        replayButton.style.width = '40px';
+        if ( closingAnimation.clientWidth >= 890 )
+            replayButton.style.width = '65px';
+    }
+}
+
+function showRepeat(e) {
+    if ( !isReplayDisplayed ) {
+        const replayButton = document.createElement('img');
+        replayButton.setAttribute('src', 'res/replay.gif');
+        replayButton.setAttribute('onclick', 'replay()');
+        replayButton.setAttribute('id', 'replayButton');
+        replayButton.style.width = '40px';
+        if ( closingAnimation.clientWidth >= 890 )
+            replayButton.style.width = '65px';
+        replayButton.style.position = 'absolute';
+        replayButton.style.top = 0;
+        replayButton.style.left = 0;
+        replayButton.style.opacity = 1;
+
+        closing.appendChild(replayButton);
+        isReplayDisplayed = true;
+    }
+}
+
+function middleSection() {
+    btnSubmit.classList.remove('disabled');
+    btnPrev.classList.add('disabled');
+    btnNext.classList.add('disabled');
+    opening.style.opacity = 0;
+    envelope.style.opacity = 0;
+    paper.style.opacity = 1;
+    wrapper.style.overflow = 'visible';
+}
+
+function submitSection() {
+    btnSubmit.classList.add('disabled');
+    btnPrev.classList.remove('disabled');
+    btnNext.classList.remove('disabled');
+    paper.style.opacity = 0;
+    wrapper.style.overflow = 'hidden';
+    envelope.style.opacity = 1;
+}
+
+function verificationSection() {
+    btnSubmit.classList.add('disabled');
+    btnPrev.classList.add('disabled');
+    btnNext.classList.add('disabled');
+    isReplayDisplayed = false;
+    envelope.style.opacity = 0;
+    closing.style.opacity = 1;
+    closingAnimation.play();
+}
+
+function replay() {
+    isReplayDisplayed = false;
+    document.getElementById('replayButton').remove();
+    source.setAttribute('src', 'res/opening-scene.mp4'); // here will be the full animation
+    source.setAttribute('type', 'video/mp4');
+    closingAnimation.load();
+    closingAnimation.play();
+}
