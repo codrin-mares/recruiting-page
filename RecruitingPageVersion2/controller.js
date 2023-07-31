e23b768edb699f2153681aa9cf7c5c8b46ad7730
Codrin Mares <codrin@tactillo.com>
2023-07-31 14:10:20 +0300

    Add version with email smtp and qp ref extraction.

--- RecruitingPageVersion2/controller.js
+++ RecruitingPageVersion2/controller.js
@@ -1,17 +1,42 @@
 console.log('Hi Controller!');
 
+/* SmtpJS.com - v3.0.0 */
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+function extractRefQP() {
+  const qp = window.location.href.split('?')[1];
+
+  if (!qp) {
+    return undefined;
+  }
+
+  const res = qp.split('&').reduce((acc, item) => {
+    const [k, v] = item.split('=');
+
+    return {
+      ...acc,
+      [k]: v,
+    };
+  }, {})
+
+  return res.ref;
+}
+
 function sendEmail() {
   Email.send({
     Host: "smtp.gmail.com",
-    Username: "sender@email_address.com",
-    Password: "Enter your password",
-    To: 'receiver@email_address.com',
-    From: "sender@email_address.com",
-    Subject: "Sending Email using javascript",
+    Username: "questfoxstudio@gmail.com",
+    Password: "Airplane111!",
+    To: 'questfoxstudio@gmail.com',
+    From: "questfoxstudio@gmail.com",
+    Subject: "Test recruitment email",
     Body: "Well that was easy!!",
   })
     .then(function (message) {
       alert("mail sent successfully")
+    })
+    .catch(function(error) {
+      console.log('ERROR', error);
     });
 }
 
@@ -30,6 +55,7 @@ function handleSubmit(e) {
 
 function confirmSubmit() {
   // alert(`Email sent with: ${JSON.stringify(formValues)}`, );
+  sendEmail();
   showClosingScene();
 }
 
@@ -41,16 +67,23 @@ const openingWrapper = document.getElementById("opening-wrapper");
 const closingScene = document.getElementById("closing");
 const closingWrapper = document.getElementById("closing-wrapper");
 const replayBtn = document.getElementById("replay-btn");
+const replayBtn2 = document.getElementById("replay-btn2");
 const paper = document.getElementById("paper-wrapper");
 const envelope = document.getElementById("envelope");
+const replayScene = document.getElementById("replay-scene");
+const replaySceneWrapper = document.getElementById("replay-scene-wrapper");
 
 openingScene?.addEventListener('ended', () => {
   openingWrapper.style.display = 'none';
-  paper.style.display = 'block';
+  paper.classList.add('fade-in-transition');
 });
 
 closingScene?.addEventListener('ended', () => {
   replayBtn.style.display = 'block';
+})
+
+replayScene?.addEventListener('ended', () => {
+  replayBtn2.style.display = 'block';
 })
 
 function showEnvelope() {
@@ -64,6 +97,7 @@ function hideEnvelope() {
 function showClosingScene() {
   envelope.style.display = 'none';
   paper.style.display = 'none';
+  replayBtn.style.display = 'none';
   closingWrapper.style.display = 'flex';
   closingScene.currentTime = 0;
   closingScene.play();
@@ -71,6 +105,17 @@ function showClosingScene() {
 
 replayBtn.addEventListener('click', () => {
   replayBtn.style.display = 'none';
-  closingScene.currentTime = 0;
-  closingScene.play();
+  closingWrapper.style.display = 'none';
+  replaySceneWrapper.style.display = 'flex';
+
+  replayScene.currentTime = 0;
+  replayScene.play();
+})
+
+replayBtn2.addEventListener('click', () => {
+  replayBtn2.style.display = 'none';
+  replaySceneWrapper.style.display = 'flex';
+
+  replayScene.currentTime = 0;
+  replayScene.play();
 })
